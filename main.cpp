38d51feacbfbbf8d0cc8b887b43093542ed49e7c
kirilllivaaa <kirya.liva@mail.ru>
2018-09-07 17:54:55 +0300

    pppp

--- main.cpp
+++ main.cpp
@@ -7,4 +7,9 @@ int main() {
     std::cout << a - 3;
     std::cout << a * 3;
     std::cout << a / 3;
+    char c = 120;
+    std::cout << c + 3<<std::endl;
+    std::cout << c - 3<<std::endl;
+    std::cout << c * 3<<std::endl;
+    std::cout << c / 3<<std::endl;
 }
